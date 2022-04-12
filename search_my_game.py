2cf42d68de06fc62bf490a577bdde74181a61c4e
DimasRidhwana <88782280+DimasRidhwana@users.noreply.github.com>
2022-04-12 17:44:54 +0700

    Update search_my_game.py

--- search_my_game.py
+++ search_my_game.py
@@ -9,17 +9,17 @@ def search_my_game (arr):  # Hanya untuk User
     l = 0
 
     if Game_Id != "" and year == "":
-        for i in range (panjang(arr)):              #Range blm bener harusnya number of row
+        for i in range (panjang(arr)):              
             if arr[i][0] == Game_Id:
                 j += 1
                 return print(f'{j}.' + arr[i])
     elif Game_Id == "" and year != "":
-        for i in range(panjang(arr)):               #Range blm bener harusnya number of row
+        for i in range(panjang(arr)):               
             if arr[i][3] == year:
                 k += 1
                 return print(f'{k}.' + arr[i])
     elif Game_Id != 0 and year != 0:
-        for i in range(panjang(arr)):               #Range blm bener harusnya number of row
+        for i in range(panjang(arr)):               
             if arr[i][3] == year and arr[i][0] == Game_Id:
                 l += 1
-                return print(f'{l}.' + arr[i])          #kemungkinan cuma print 1, coba hapus return
+                return print(f'{l}.' + arr[i])          
