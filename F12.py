665939dd891267b36b5adef4e93946bd11fac68f
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-12 19:40:44 +0700

    Update F12.py

--- F12.py
+++ F12.py
@@ -1,22 +1,21 @@
-
 from modul_fungsi.array_function import panjang
 
-def Exist(arr,data, column):		    # Validasi apakah data ada
-	i = 0
-	for i in range(panjang(arr)):
-		LookUp = arr[i][column]
-		if LookUp == data:
-			return True
-		else:
-			return False
+def ada(user,username,kolom):
+    i=0
+    while i<panjang(user):
+        if user[i][kolom]==username:
+            break
+        return True
 
 username=input('Masukkan username: ')
+
 topup=int(input('Masukkan saldo: '))
 
-user=[['001','mikdar','ghaylan','1234','user',30000],['002','zenn','fatih','4321','user',20000]]
+user=[['001','mikdar','ghaylan','1234','user',30000],['002','zenn','fatih','4321','user',20000],['003','owo','alif','2323','user',40000]]
 
-if Exist(user,username,1)==False:
-    print(f'Username “{username}” tidak ditemukan.')
+if ada(user,username,1)==False:
+    print(f'Username {username} tidak ditemukan.')
+
 
 for i in range (panjang(user)):
     if user[i][1] == username:
@@ -25,4 +24,5 @@ for i in range (panjang(user)):
         else:
             print(f'Top up berhasil. Saldo {username} bertambah menjadi {user[i][5] + topup}')
             user[i][5]= user[i][5]+ topup
+
 print(user)
