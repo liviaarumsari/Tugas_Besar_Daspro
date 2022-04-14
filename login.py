9af1cfdb737f6f78ae1d7a5e8d0ba8776ac0dd6b
felisadarmawan <89518053+felisadarmawan@users.noreply.github.com>
2022-04-14 13:32:06 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 from modul_fungsi.array_function import panjang 
 
 currentuser = False
-def login(data):
+def login(data_f):
     if currentuser = True:
         print("Sudah login, masukan perintah selanjutnya.")
     else:
@@ -9,15 +9,15 @@ def login(data):
         password= input("Masukan password: ")
 
 
-        for i in range(panjang(data)):
-            if data[i][1] == username:
-                if data[i][3] == password:
+        for i in range(panjang(data_f)):
+            if data_f[i][1] == username:
+                if data_f[i][3] == password:
                     print("Halo {}!, Selamat datang di 'Binomo'.".format(data_f[i][2]))
                     currentuser = True #tandanya udah login
                     break
                 else:
                     print("Password atau username salah atau tidak ditemukan.")
-            if i==panjang(data-1):
+            if i==panjang(data_f-1):
                 print("Password atau username salah atau tidak ditemukan.")
     
 
