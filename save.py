f9c0c1c5698e043e72469f590ba4ea27d00aec03
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-15 22:44:37 +0700

    Update and rename F16.py to save.py

--- save.py
+++ save.py
@@ -9,7 +9,7 @@ def panjang(arr):
         i += 1
     return i
 def save_data(data, nama_folder, nama_file):
-    file=open("./%s/%s.csv" % (nama_folder, nama_file), "w")    
+    file=open("./%s/%s.csv" % (nama_folder, nama_file), "w")    #fungsi save_data berbeda dari di modul fungsi
     for i in range(panjang(data)):
         for j in range(panjang(data[0])):
             if (j == panjang(data[0])-1):
@@ -22,7 +22,7 @@ def save_data(data, nama_folder, nama_file):
 def save(kepemilikan,user,game,riwayat):
 
     kepemilikan= [['G010','002'],['G002','002'],['G033','002']]
-    user=[['001','mikdar','ghaylan','1234','user',30000],['002','zenn','fatih','4321','user',20000]]
+    user=[['001','mikdar','ghaylan','1234','user',30000],['002','zenn','fatih','4321','user',20000]]  #CONTOH DATA, HAPUS SAAT PEMAKAIAN
     game=[['G001','pyhton','action','2015',25000,25],['G002','dota','action','2015',20000,2]]
     riwayat=[['G001','python','001',2022],['G002','dota','001',2021],['G001','python','002',2022]]
 
