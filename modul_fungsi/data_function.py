73d627e3ac2c3ca5066a97d38fe3bcaa9a876b8d
DimasRidhwana <88782280+DimasRidhwana@users.noreply.github.com>
2022-04-18 20:52:01 +0700

    Tambah Linear Congruential Generator

--- modul_fungsi/data_function.py
+++ modul_fungsi/data_function.py
@@ -59,3 +59,21 @@ def isAdmin(arr,username):				# Cek Apakah Admin
 	else:
 		return False
 print(gameID(1))
+
+
+def LCG(N,S):               # linear congruential generator (LCG)
+    a = 7**3
+    M = 2**5-1
+
+    def f(S):
+        return (a*S) % M
+    
+    U = 0
+    for i in range(N):
+        S = f(S)
+        U += (S/M)
+    return U
+
+
+
+
