a39ae4403d78072a9b840de5afb9e37e8a211f02
DimasRidhwana <88782280+DimasRidhwana@users.noreply.github.com>
2022-04-19 12:50:03 +0700

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,31 @@
+ def TicTacToe():
+  board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+  ]
+
+  user = True # when true it refers to x, otherwise o
+  turns = 0
+
+  while turns < 9:
+    active_user = CurrentUser(user)
+    Board(board)
+    user_input = input("Masukkan posisi 1 sampai 9 atau masukkan \"q\" untuk berhenti bermain:")
+    if QuitGame(user_input): break
+    if not IsInputValid(user_input):
+      print("Coba lagi.")
+      continue
+    user_input = int(user_input) - 1
+    coords = Coor(user_input)
+    if IsTaken(coords, board):
+      print("Coba lagi.")
+      continue
+    IsiBoard(coords, board, active_user)
+    if IsWin(active_user, board): 
+      print(f"{active_user.upper()} Menang!")
+      break
+    
+    turns += 1
+    if turns == 9: print("Seri!")
+    user = not user
