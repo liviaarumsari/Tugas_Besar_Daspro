c101304700b9eb83a63ab237d87fe001ccb1b56a
DimasRidhwana <88782280+DimasRidhwana@users.noreply.github.com>
2022-04-20 10:17:49 +0700

    Tambah fungsi formatCariGame

--- modul_fungsi/array_function.py
+++ modul_fungsi/array_function.py
@@ -105,7 +105,43 @@ def maxchar(arr, column):
     return max
 
 
-#
+def formatCariGame(arr):
+    # from
+    # [['G001','Binomo','Judi Online', '2020', 10000, 10],['G002','kacang','Garing', '2020', 10000, 10]]
+    # to
+    # G001 | Binomo | 10.000 | Judi Online | 2022
+    # G002 | kacang | 10.000 | Garing | 2022
+    
+    text = ""
+    numbering = 1
+    for i in range(panjang(arr)):
+        gameID = str(arr[i][0])
+        nama = str(arr[i][1])
+        harga =  str(arr[i][4])
+        kategori = str(arr[i][2])
+        tahun = str(arr[i][3])
+
+        if panjang(splitchar(gameID)) != maxchar(arr,0):
+            space = " " * (maxchar(arr,0) - (panjang(splitchar(gameID))))
+            gameID += space
+        if panjang(splitchar(nama)) != maxchar(arr,1):
+            space = " " * (maxchar(arr,1) - (panjang(splitchar(nama))))
+            nama += space
+        if panjang(splitchar(harga)) != maxchar(arr,4):
+            space = " " * (maxchar(arr,4) - (panjang(splitchar(harga))))
+            harga += space
+        if panjang(splitchar(kategori)) != maxchar(arr,2):
+            space = " " * (maxchar(arr,2) - (panjang(splitchar(kategori))))
+            kategori += space
+        if panjang(splitchar(tahun)) != maxchar(arr,3):
+            space = " " * (maxchar(arr,3) - (panjang(splitchar(tahun))))
+            tahun += space
+        
+        newtext = f"{str(numbering)}. {gameID} | {nama} | {harga} | {kategori} | {tahun} \n"
+        text += newtext
+        numbering += 1
+    
+    return text
 
 
 
