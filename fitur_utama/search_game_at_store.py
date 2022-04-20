6b2eb30cfa328392a3252d918bc7dad89a176771
felisadarmawan <89518053+felisadarmawan@users.noreply.github.com>
2022-04-20 13:45:32 +0700

    Update search_game_at_store.py

--- fitur_utama/search_game_at_store.py
+++ fitur_utama/search_game_at_store.py
@@ -11,39 +11,6 @@ def search_game_at_store (data):
         kategori = input("Masukkan Kategori Game : ")
         tahun = input("Masukkan Tahun Rilis Game : ")
 
-        a=0
-        b=0
-        c=0
-        d=0
-        e=0
-        f=0
-        g=0
-        h=0
-        i=0
-        j=0
-        k=0
-        l=0
-        m=0
-        n=0
-        o=0
-        p=0
-        q=0
-        r=0
-        s=0
-        t=0
-        u=0
-        v=0
-        w=0
-        x=0
-        y=0
-        z=0
-        aa=0
-        ab=0
-        ac=0
-        ad=0
-        ae=0
-        af=0
-
         if idgame=="" and nama=="" and harga=="" and kategori=="" and tahun=="":
             for i in range(panjang(data)):
                 a+=1
