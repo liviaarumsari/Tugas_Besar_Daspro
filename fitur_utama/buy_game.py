b75e9bec495e604f69df11c726af2f8470cb334e
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-21 21:56:36 +0700

    Update buy_game.py

--- fitur_utama/buy_game.py
+++ fitur_utama/buy_game.py
@@ -23,16 +23,22 @@ def userID(int):
 	else:
 		return str(int)
 
-kepemilikan= [['G001','002'],['G002','002'],['G033','002']]
-user=[['001','mikdar','ghaylan','1234','user',30000],['002','zenn','fatih','4321','user',20000]] #CONTOH DATA, HAPUS SAAT PEMAKAIAN
-game=[['G001','pyhton','action','2015',25000,25],['G002','dota','action','2015',20000,2]]
-riwayat=[['G001','python','001',2022],['G002','dota','001',2022],['G001','python','002',2022]]
-def buy_game(kepemilikan,user,game,riwayat):
+def removeRow(data):
+    data_baru=[]
+    for i in range(panjang(data)):
+        if data[i]!=['']:
+            data_baru=data_baru+[data[i]]
+    return data_baru
+
+def buy_game(kepemilikan,user,game,riwayat,username):
+    kepemilikan=removeRow(kepemilikan)
+    user=removeRow(user)
+    riwayat=removeRow(riwayat)
+    game=removeRow(game)
     beli=(input('Masukkan id game: '))
-    username=input('Masukkan username: ')
-    id= userID(DataLoc(user,username,1)+1)
+    id= (DataLoc(user,username,1))
     split=splitchar(beli)
-    pembelian=[beli,id]
+    pembelian=[beli,userID(id)]
     if split[1] == '0' and split[2]=='0':
         codegame=int(split[3])
     elif split[1]=='0' and split[2]!=0:
@@ -40,32 +46,28 @@ def buy_game(kepemilikan,user,game,riwayat):
     else:
         codegame=int(split[1]+split[2]+split[3])
 
-    split2=splitchar(id)
-    if split2[0] == '0' and split2[1]=='0':
-        id2=int(split[2])
-    elif split2[0]=='0' and split2[1]!=0:
-        id2=int(split2[1]+split2[2])
-    else:
-        id2=int(split2[0]+split2[1]+split2[2])
-
     Exist=False
 
     for j in range(panjang(kepemilikan)):
         if pembelian==kepemilikan[j]:
             Exist=True
-            break
+
     if Exist==True:          
         print('Anda sudah memiliki game tersebut')
-    elif game[codegame-1][5]==0: 
+    elif int(game[codegame][5])==0: 
         print('Stok Game tersebut sedang habis!')
-    elif game[codegame-1][4] > user[id2][5]: 
+    elif int(game[codegame][4]) > int(user[id][5]): 
         print('Saldo anda tidak cukup untuk membeli Game tersebut!')
     else: 
-        game[codegame-1][5]=game[codegame-1][5]-1
-        user[id2][5]=user[id2][5]-game[codegame-1][4]
+        (game[codegame][5])=int(game[codegame][5])-1
+        user[id][5]=str(int(user[id][5])-int(game[codegame][4]))
         kepemilikan=kepemilikan+[pembelian]
-        riwayat=riwayat+[beli,game[codegame][1],2022] #input tahun beli msh manual
+        riwayat=riwayat+[[beli,game[codegame][1],game[codegame][4],userID(id),2022]] #input tahun beli msh manual
         print('Game',f'"{game[codegame][1]}"','berhasil dibeli')
-    return game,user,kepemilikan,riwayat
+
+
+
+
+    return (kepemilikan,user,game,riwayat)
 
 
