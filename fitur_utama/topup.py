a33224378ec84254dcb83ef70594a1cde20cb7e8
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-21 21:56:04 +0700

    Update topup.py

--- fitur_utama/topup.py
+++ fitur_utama/topup.py
@@ -1,25 +1,32 @@
 from modul_fungsi.array_function import panjang
 
+def removeRow(data):
+    data_baru=[]
+    for i in range(panjang(data)):
+        if data[i]!=['']:
+            data_baru=data_baru+[data[i]]
+    return data_baru
+
 def topup(user):   #user=data
+    print(user)
+    user=removeRow(user)
+    print(user)
     username=input('Masukkan username: ')
 
     topup=int(input('Masukkan saldo: '))
 
-    Exist=False
 
-    for j in range(panjang(user)):
-        if user[j][1] == username:        
-            Exist=True
-            break
-    if Exist==True:
-        for i in range (panjang(user)):
-            if user[i+1][1] == username:
-                if (user[i+1][5] + topup)<0:
-                    print('Masukan tidak valid')
-                else:
-                    print(f'Top up berhasil. Saldo {username} bertambah menjadi {user[i+1][5] + topup}')
-                    user[i+1][5]= user[i+1][5]+ topup
-    else:
-        print(f'Username {username} tidak ditemukan.')
+    for i in range (panjang(user)):
+        if user[i][1] == username:
+            if (int(user[i][5]) + topup)<0:
+                print('Masukan tidak valid')
+                break
+            elif i==(panjang(user)-1) and user[i][1]!=username:
+                print(f'Username {username} tidak ditemukan.')
+            else:
+                print(f'Top up berhasil. Saldo {username} bertambah menjadi {int(user[i][5]) + topup}')
+                user[i][5]= int(user[i][5])+ topup
+                user[i][5]= str(user[i][5])
+                print(user)
     return user
        
