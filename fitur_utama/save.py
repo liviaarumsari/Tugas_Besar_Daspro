8427d75f57c952fd5ceb755aa9343a3de20e222e
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-21 21:55:37 +0700

    Update save.py

--- fitur_utama/save.py
+++ fitur_utama/save.py
@@ -1,50 +1,50 @@
 import os
 def isEmpty(n):
-    n != ''
-    return n
+    if n != '':
+        return n
 
 def panjang(arr):
     i = 0
     for c in arr:
         i += 1
     return i
+# Matriks to CSV
 def save_data(data, nama_folder, nama_file):
-    file=open("./%s/%s.csv" % (nama_folder, nama_file), "w")    #fungsi save_data berbeda dari di modul fungsi
+    file=open(f"{nama_folder}"+f"/{nama_file}.csv", "w")    
     for i in range(panjang(data)):
         for j in range(panjang(data[0])):
             if (j == panjang(data[0])-1):
-                file.write(str(data[i][j]))
+                file.write(str(data[i][j])+'\n')
             else:
-                file.write(str(data[i][j]) + ";")
-        file.write("\n")
+                file.write(str(data[i][j]+';'))
     file.close()
 
+def removeRow(data):
+    data_baru=[]
+    for i in range(panjang(data)):
+        if data[i]!=['']:
+            data_baru=data_baru+[data[i]]
+    return data_baru
+
 def save(kepemilikan,user,game,riwayat):
-
-    kepemilikan= [['G010','002'],['G002','002'],['G033','002']]
-    user=[['001','mikdar','ghaylan','1234','user',30000],['002','zenn','fatih','4321','user',20000]]  #CONTOH DATA, HAPUS SAAT PEMAKAIAN
-    game=[['G001','pyhton','action','2015',25000,25],['G002','dota','action','2015',20000,2]]
-    riwayat=[['G001','python','001',2022],['G002','dota','001',2021],['G001','python','002',2022]]
-
+    kepemilikan=removeRow(kepemilikan)
+    user=removeRow(user)
+    riwayat=removeRow(riwayat)
+    game=removeRow(game)
 
     folder=input('Masukkan nama folder penyimpanan: ')
     while not isEmpty(folder):
         folder=input('Masukkan nama folder penyimpanan: ')
-    path=os.path.join('D:/tubes daspro/Tugas_Besar_Daspro-main/',folder)
 
-    if os.path.isdir(path)==False:
-        os.mkdir(path)
-        save_data(game,folder,'game')
-        save_data(user,folder,'user')
-        save_data(riwayat,folder,'riwayat')
-        save_data(kepemilikan,folder,'kepemilikan')
-        print(f'Data telah disimpan pada folder {folder}')
-    else:
-        save_data(game,folder,'game')
-        save_data(user,folder,'user')
-        save_data(riwayat,folder,'riwayat')
-        save_data(kepemilikan,folder,'kepemilikan')
-        print(f'Data telah disimpan pada folder {folder}')
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    save_data(game,folder,'game')
+    save_data(riwayat,folder,'riwayat')
+    save_data(kepemilikan,folder,'kepemilikan')
+    save_data(user,folder,'user')
+    print('saving...')
+    print(f'Data telah disimpan pada folder {folder}')
 
 
 
@@ -61,4 +61,4 @@ def save(kepemilikan,user,game,riwayat):
 
 
 
-print('saving...')
+
