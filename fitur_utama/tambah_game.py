fce2835a539b7bd34c7e4533dafe62aa13294015
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-21 21:54:55 +0700

    Update tambah_game.py

--- fitur_utama/tambah_game.py
+++ fitur_utama/tambah_game.py
@@ -2,11 +2,24 @@ from modul_fungsi.array_function import panjang
 from modul_fungsi.data_function import gameID
 
 def isEmpty(n):
-    n != ''
-    return n
+    if n == '':
+        return False
+    else:
+        return n
 
+def removeRow(data):
+    data_baru=[]
+    for i in range(panjang(data)):
+        if  data[i]!=['']:
+            data_baru=data_baru+[data[i]]
+        elif data[i]==['','','','']:
+            data_baru=data_baru+[data[i]]
+    return data_baru
 
 def tambah_game(data):
+    print(data)
+    data=removeRow(data)
+    print(data)
     namaGame=input('Masukkan nama game: ')
     kategori=input('Masukkan kategori: ')
     tahunRilis=(input('Masukkan tahun rilis: '))
@@ -22,8 +35,9 @@ def tambah_game(data):
             harga=(input('Masukkan harga: '))
             stok=(input('Masukkan stok awal: '))
         else:
-            arr=[gameID(panjang(data)+1),namaGame, kategori, tahunRilis, harga, stok]
-            data_baru = data + [arr]
-            print(f'Selamat! Berhasil menambahkan game {namaGame}.')       
-            return(data_baru)
+            arr=[gameID(panjang(data)),namaGame,kategori,tahunRilis,harga,stok]
+            data = data + [arr]
+            print(f'Selamat! Berhasil menambahkan game {namaGame}.') 
+            print(data)      
+            return(data)
 
