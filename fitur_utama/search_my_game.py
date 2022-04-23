77defa140c99ffd6f48c921bf4275da4f29c81b2
DimasRidhwana <88782280+DimasRidhwana@users.noreply.github.com>
2022-04-23 11:42:50 +0700

    Update search_my_game.py

--- fitur_utama/search_my_game.py
+++ fitur_utama/search_my_game.py
@@ -15,31 +15,31 @@ def search_my_game(username, user, kepemilikan, game):
     game_user = data_game_user(username, user, kepemilikan)
 
     filtered_arr = []
-    if Game_Id != "" and year == "":                # Hanya input Game id 
-        if Exist(game, Game_Id, 0) == True:        # Cek apakah game ada di toko
-            if game_owned(Game_Id, game_user) == True:        # Cek apakah user punya game itu
+    if Game_Id != "" and year == "":                            # Hanya input Game id 
+        if Exist(game, Game_Id, 0) == True:                     # Cek apakah game ada di toko
+            if game_owned(Game_Id, game_user) == True:          # Cek apakah user punya game itu
                 for i in range(panjang(game)):
                     if game[i][0] == Game_Id:      
-                        filtered_arr += [game[i]]     # Buat array yang sudah terfilter sesuai kriteria input user 
+                        filtered_arr += [game[i]]               # Buat array yang sudah terfilter sesuai kriteria input user 
 
 
-    elif Game_Id == "" and year != "":              # Hanya input tahun
+    elif Game_Id == "" and year != "":                          # Hanya input tahun
         if Exist(game, year, 3) == True:
-            temp_arr = []                               # Array berisi spek dengan tahun tertentu
+            temp_arr = []                                       # Array berisi spek dengan tahun tertentu
             for i in range(panjang(game)):
                 if game[i][3] == year:
                     temp_arr += [game[i]]
                     
-            if not(temp_arr == []):                             # Cek apakah game ada di toko
+            if not(temp_arr == []):                                         # Cek apakah game ada di toko
                 for i in range(panjang(temp_arr)):
-                    if game_owned(temp_arr[i][0], game_user) == True:     # Cek apakah user punya game itu     
-                        filtered_arr += [game[i]]              # Buat array yang sudah terfilter sesuai kriteria input user
+                    if game_owned(temp_arr[i][0], game_user) == True:       # Cek apakah user punya game itu     
+                        filtered_arr += [temp_arr[i]]                           # Buat array yang sudah terfilter sesuai kriteria input user
 
 
-    elif Game_Id != "" and year != "":              # input Game id + tahun
+    elif Game_Id != "" and year != "":                                          # input Game id + tahun
         if Exist(game, year, 3) == True and Exist(game, Game_Id, 0) == True:
-            temp_arr1 = []                      # Array berisi spek dengan tahun tertentu
-            temp_arr2 = []                      # Array berisi spek dengan tahun dan game id tertentu
+            temp_arr1 = []                                                      # Array berisi spek dengan tahun tertentu
+            temp_arr2 = []                                                      # Array berisi spek dengan tahun dan game id tertentu
 
             for i in range(panjang(game)):
                 if game[i][3] == year:
@@ -49,10 +49,10 @@ def search_my_game(username, user, kepemilikan, game):
                 if temp_arr1[i][0] == Game_Id:
                     temp_arr2 += [temp_arr1[i]]
 
-            if not(temp_arr2 == []):                             # Cek apakah game ada di toko
+            if not(temp_arr2 == []):                                            # Cek apakah game ada di toko
                 for i in range(panjang(temp_arr2)):
-                    if game_owned(temp_arr2[i][0], game_user) == True:     # Cek apakah user punya game itu     
-                        filtered_arr += [game[i]]              # Buat array yang sudah terfilter sesuai kriteria input user
+                    if game_owned(temp_arr2[i][0], game_user) == True:          # Cek apakah user punya game itu     
+                        filtered_arr += [temp_arr2[i]]                          # Buat array yang sudah terfilter sesuai kriteria input user
 
 
     if filtered_arr == []:
