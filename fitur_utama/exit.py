4ea327de8b9be11deaf8dfd9af4acdf294bd9134
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2022-04-24 05:58:59 +0700

    Revisi exit\

--- fitur_utama/exit.py
+++ fitur_utama/exit.py
@@ -6,7 +6,7 @@ def exit(kepemilikan, user, game, riwayat):
     while (i == False):
         print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
         opt = input()
-        if (opt == "y") or (opt == "Y") or (opt == "n") or (opt == "y"):
+        if (opt == "y") or (opt == "Y") or (opt == "n") or (opt == "N"):
             i = True
     if (opt == "y") or (opt == "Y"):
         # Jalankan fungsi save
