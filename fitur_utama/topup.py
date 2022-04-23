34521ade1961aea8666eb54238a1bc3b79fd34d5
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2022-04-23 16:17:43 +0700

    Update topup.py

--- fitur_utama/topup.py
+++ fitur_utama/topup.py
@@ -1,5 +1,11 @@
 from modul_fungsi.array_function import panjang
 
+def isEmpty(n):
+    if n == '':
+        return False
+    else:
+        return n
+
 def removeRow(data):
     data_baru=[]
     for i in range(panjang(data)):
@@ -9,22 +15,23 @@ def removeRow(data):
 
 def topup(user):   #user=data
     user=removeRow(user)
-    username=input('Masukkan username: ')
-
-    topup=int(input('Masukkan saldo: '))
-
+    username=''
+    topup=''
+    while not isEmpty(user) or not isEmpty(topup):
+        username=input('Masukkan username: ')
+        topup=(input('Masukkan saldo: '))
 
     for i in range (panjang(user)):
         if user[i][1] == username:
-            if (int(user[i][5]) + topup)<0:
+            if (int(user[i][5]) + int(topup))<0:
                 print('Masukan tidak valid')
                 break
             elif i==(panjang(user)-1) and user[i][1]!=username:
                 print(f'Username {username} tidak ditemukan.')
             else:
-                print(f'Top up berhasil. Saldo {username} bertambah menjadi {int(user[i][5]) + topup}')
-                user[i][5]= int(user[i][5])+ topup
+                print(f'Top up berhasil. Saldo {username} bertambah menjadi {int(user[i][5]) + int(topup)}')
+                user[i][5]= int(user[i][5])+ int(topup)
                 user[i][5]= str(user[i][5])
-                
+                print(user)
     return user
        
