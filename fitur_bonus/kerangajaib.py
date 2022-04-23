de1abf9e2619101280279a430babf49baf80ca73
DimasRidhwana <88782280+DimasRidhwana@users.noreply.github.com>
2022-04-23 11:24:44 +0700

    Update kerangajaib.py

--- fitur_bonus/kerangajaib.py
+++ fitur_bonus/kerangajaib.py
@@ -6,9 +6,9 @@ def kerangajaib():
     input("Apa pertanyaanmu? ")
     number = time.time()
     if 0 <= LCG(1,number) <= 0.33:
-        return "Tidak"
+        print("Tidak")
     elif 0.33 < LCG(1,number) <= 0.67:
-        return "Mungkin"
+        print("Mungkin")
     else:
-        return "Ya"
+        print("Ya")
 
